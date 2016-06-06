7c90b3e4c793b4c7e1ecebefaf2f09d546dd9553
James Strachan <james.strachan@gmail.com>
2016-06-06 08:44:47 +0100

    added slightly more complex funktion example

--- hello-rest.js
+++ hello-rest.js
@@ -1,6 +1,13 @@
 var http = require('http');
+var url = require('url');
 http.createServer(function (req, res) {
+  var query = url.parse(req.url, true).query;
+  var name = query.name;
+  var message = 'Please pass ?name=foo to specify your name!\n';
+  if (name) {
+    message = 'Hello ' + name + ' from a nodejs funktion!\n';
+  }
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from a nodes funktion\n');
+  res.end(message + 'I am running on host ' + process.env.HOSTNAME);
 }).listen(8080);
 console.log('Server running at http://localhost:9090/');
